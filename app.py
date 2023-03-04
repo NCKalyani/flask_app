48679190bbd88068bda211484d4406b25b472fe4
kalyani <my@example.com>
2023-03-04 16:27:11 +0530

    adding new file

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, request, url_for
+from http import HTTPStatus
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<h1>welcome to cooking</h1>"
+
+
+recipes = [
+    {"id": 1,
+     "name": "tomato soup",
+     "description": "This is a lovely tomato soup"},
+    {"id": 2,
+     "name": "egg salad",
+     "description": "this is a tasty egg salad"}
+]
+
+
+@app.route('/recipes', methods=["GET"])
+def get_recipes():
+    return jsonify({"data": recipes})
+
+
+@app.route('/recipes/<int:recipe_id>', methods=['GET'])
+def get_recipe(recipe_id):
+    recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_id), None)
+    if recipe:
+        return jsonify(recipe)
+    return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_FOUND
+
+
+@app.route('/recipes', methods=['POST'])
+def create_recipe():
+    recipe = {
+      'id': len(recipes) + 1,
+      'name': request.args.get('name'),
+      'description': request.args.get('description')
+    }
+    recipes.append(recipe)
+    url_for()
+    return jsonify(recipe), HTTPStatus.CREATED
+# @app.route('/recipes/<int:recipe_id>', methods=['PUT'])
+# def update_recipe(recipe_id):
+#  recipe = next((recipe for recipe in recipes if recipe['id'] == recipe_
+# id), None)
+#  if not recipe:
+# 20 | Your First Step
+#  return jsonify({'message': 'recipe not found'}), HTTPStatus.NOT_
+# FOUND
+#     data = request.get_json()
+#  recipe.update(
+#  {
+#  'name': data.get('name'),
+#  'description': data.get('description')
+#  }
+#  )
+#  return jsonify(recipe)
+
+
+if __name__ == '__main__':
+    app.run(Debug=True)
